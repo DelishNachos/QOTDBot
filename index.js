c6204d2280560172a5ec732da3de0fd1b367ee81
DelishNachos <ipfagan6@gmail.com>
2025-01-01 11:12:13 -0700

    new date

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   
   // Schedule the daily quote message
-  schedule.scheduleJob({ hour: 11, minute: 12, tz: timezone }, async () => {
+  schedule.scheduleJob({ hour: 11, minute: 13, tz: timezone }, async () => {
     const channel = client.channels.cache.get(DAILY_CHANNEL_ID);
     if (channel) {
       const quote = await getQuoteForToday();
